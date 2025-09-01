f564c400244614ab1a5581c1e1a4bcf037df16a2
Ignacio <ignaciobuffaz73@gmail.com>
2025-09-01 19:13:19 -0300

    se agrego axios y se configuro

---
+++ src/services/api.config.ts
@@ -0,0 +1,7 @@
+import { API_URL } from "@/config/env";
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: API_URL,
+  headers: { "Content-Type": "application/json" },
+});
